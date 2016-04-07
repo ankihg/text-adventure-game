4469be8d1e67ee681416e82f9467656e3c53104b
ankihg <hagelin.annika@gmail.com>
2016-04-07 10:29:31 -0700

    player switch

--- es5/lib/game.js
+++ es5/lib/game.js
@@ -8,22 +8,30 @@ var rl = readline.createInterface({
 // init game
 var Game = module.exports = function(world) {
   this.world = world; //has constructors of objects found in world
+  this.players = [];
+  this.turns = 0;
 }
 
 Game.prototype.play = function() {
-  this.prompt(this.world.Creature.firstBorn);
+  this.currentPlayer = this.players[this.turns % this.players.length];
+  this.prompt(this.currentPlayer);
 
   var game = this;
   this.read(function(response) {
     console.log('got line '+response);
 
+    game.turns++;
     game.play();
   });
 }
 
 Game.prototype.makeRealWorld = function() {
   // creatures
-  var firstBorn = this.world.Creature.firstBorn = new this.world.Creature('i was born first');
+  var firstBorn = this.world.Creature.firstBorn = new this.world.Creature('firstBorn');
+  this.players.push(firstBorn);
+  var tad = this.world.Creature.tad = new this.world.Creature('tad');
+  this.players.push(tad);
+
 
   // locations
   var kellis = this.world.Location.Bar.kellis = new this.world.Location.Bar('kellis', [new this.world.Location.MenuItem(this.world.Item.Beverage.AlcoholicBeverage.PBR, 2)]);
